b183e651812660fa02e87360d2738624c7d4d278
Juanka <valerojuanka@gmail.com>
2022-04-04 20:08:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Navbar, Header, Footer} from "./components"
+
+function App() {
+
+  return (
+    <main>
+      <header className="header-bg">
+        <Navbar />
+        <Header />
+      </header>
+    </main>
+  )
+}
+
+export default App
